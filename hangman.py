7d515ef9fb26c2198b59c95a765ec20a89526e4e
chienleow <chienleow1@gmail.com>
2021-02-23 14:21:30 -0500

    decrease letter once guessed by user

--- hangman.py
+++ hangman.py
@@ -14,6 +14,10 @@ def hangman():
 
     # getting user input
     user_letter = input('Enter a letter:').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
 
 user_input = input('Enter a letter:')
 print(user_input)
